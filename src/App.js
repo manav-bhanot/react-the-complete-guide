16b9e3de40e5b1d75b29e3b31617e77304fc4ff1
Manav Bhanot <manavb@amazon.com>
2022-07-15 15:17:15 -0700

    L38: Adding 'normal' JS logic to components

--- src/App.js
+++ src/App.js
@@ -12,7 +12,12 @@ const expenses = [
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e2",
+    title: "New TV",
+    amount: 799.49,
+    date: new Date(2021, 2, 12),
+  },
   {
     id: "e3",
     title: "Car Insurance",
@@ -45,6 +50,11 @@ function App() {
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date.toISOString()}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date.toISOString()}
       />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
 
   return (
     <div className="expense-item">
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div>{props.date}</div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
