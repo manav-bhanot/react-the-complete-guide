be8f0194e978521cf05caced7505e6060743d9b4
Manav Bhanot <manavb@amazon.com>
2022-07-15 16:54:58 -0700

    L39: Splitting components into multiple sub-components

--- src/App.js
+++ src/App.js
@@ -39,22 +39,22 @@ function App() {
       <ExpenseItem
         title={expenses[0].title}
         amount={expenses[0].amount}
-        date={expenses[0].date.toISOString()}
+        date={expenses[0].date}
       />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
-        date={expenses[1].date.toISOString()}
+        date={expenses[1].date}
       />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
-        date={expenses[2].date.toISOString()}
+        date={expenses[2].date}
       />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
-        date={expenses[3].date.toISOString()}
+        date={expenses[3].date}
       />
     </div>
   );
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,24 +1,16 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
-  const year = props.date.getFullYear();
-
-  return (
-    <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
-      <div>{props.date}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">{props.amount}</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="expense-item">
+			<ExpenseDate date={props.date} />
+			<div className="expense-item__description">
+				<h2>{props.title}</h2>
+				<div className="expense-item__price">${props.amount}</div>
+			</div>
+		</div>
+	);
 }
 
 export default ExpenseItem;
