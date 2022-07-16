a0d477f3e82e32ae36ebfc59c1a1fcb81faecfa4
Manav Bhanot <manavb@amazon.com>
2022-07-15 23:20:47 -0700

    Assignment1: React and Component Basics

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@
 
 // NOTE: Custom components name MUST start with a UPPER CASE character
 
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 const expenses = [
   {
@@ -35,27 +35,7 @@ const expenses = [
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses expenses={expenses}/>
     </div>
   );
 }
---
+++ src/components/Expenses.js
@@ -0,0 +1,33 @@
+import './Expenses.css'
+import ExpenseItem from './ExpenseItem';
+
+function Expenses(props) {
+  console.log(props)
+  return (
+    <div className="expenses">
+      <h2>Let's get started!</h2>
+      <ExpenseItem
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItem
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      />
+      <ExpenseItem
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      />
+    </div>
+  )
+}
+
+export default Expenses;
