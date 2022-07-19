6e4e16075b0621b6c862207681bdc60791617aca
Manav Bhanot <manavb@amazon.com>
2022-07-19 16:44:02 -0700

    L68: Adding Conditional Return Statement

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 import "./Expenses.css";
-import ExpenseItem from "./ExpenseItem";
 import Card from "../ui/Card";
 import ExpensesFilter from "./ExpenseFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
 	const [filteredYear, setFilteredYear] = useState("2020");
@@ -11,43 +11,20 @@ const Expenses = (props) => {
 	const filterExpensesByYear = (selectedYear) => {
 		setFilteredYear(selectedYear);
 	};
-
-	const filteredExpenses = props.items.filter(expenseItem => expenseItem.date.getFullYear().toString() === filteredYear);
-
-	let expensesContent = <p>No expenses found.</p>
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map(expenseItem => {
-			return (
-				<ExpenseItem
-					key={expenseItem.id}
-					title={expenseItem.title}
-					amount={expenseItem.amount}
-					date={expenseItem.date}
-				/>
-			)
-		});
-	}
+	const filteredExpenses = props.items.filter(
+		(expenseItem) => expenseItem.date.getFullYear().toString() === filteredYear
+	);
 
 	return (
-		<Card className="expenses">
-			<ExpensesFilter
-				onChangeYear={filterExpensesByYear}
-				selectedYear={filteredYear}
-			/>
-			{expensesContent}
-			{/* {filteredExpenses.length === 0 && <p>No expenses found.</p>}
-			{filteredExpenses.length > 0 && filteredExpenses.map(expenseItem => {
-				return (
-					<ExpenseItem
-						key={expenseItem.id}
-						title={expenseItem.title}
-						amount={expenseItem.amount}
-						date={expenseItem.date}
-					/>
-				);
-			})} */}
-		</Card>
+		<li>
+			<Card className="expenses">
+				<ExpensesFilter
+					onChangeYear={filterExpensesByYear}
+					selectedYear={filteredYear}
+				/>
+				<ExpensesList items={filteredExpenses} />
+			</Card>
+		</li>
 	);
 };
 
