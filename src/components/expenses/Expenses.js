886884eb0686c327f59b8c0f1dae310598617fe0
Manav Bhanot <manavb@amazon.com>
2022-07-19 12:32:39 -0700

    L64,65: Rendering list of data in custom components. Making list stateful to update component when items are upserted

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -6,37 +6,26 @@ import Card from "../ui/Card";
 import ExpensesFilter from "./ExpenseFilter";
 
 const Expenses = (props) => {
-
-  const [filteredYear, setFilteredYear] = useState('2020');
-
+	const [filteredYear, setFilteredYear] = useState("2020");
+	
 	const filterExpensesByYear = (selectedYear) => {
 		console.log("In Expenses.js: Selected year = ", selectedYear);
-    setFilteredYear(selectedYear);
+		setFilteredYear(selectedYear);
 	};
 
 	return (
 		<Card className="expenses">
-			<ExpensesFilter onChangeYear={filterExpensesByYear} selectedYear={filteredYear} />
-			<ExpenseItem
-				title={props.items[0].title}
-				amount={props.items[0].amount}
-				date={props.items[0].date}
+			<ExpensesFilter
+				onChangeYear={filterExpensesByYear}
+				selectedYear={filteredYear}
 			/>
-			<ExpenseItem
-				title={props.items[1].title}
-				amount={props.items[1].amount}
-				date={props.items[1].date}
-			/>
-			<ExpenseItem
-				title={props.items[2].title}
-				amount={props.items[2].amount}
-				date={props.items[2].date}
-			/>
-			<ExpenseItem
-				title={props.items[3].title}
-				amount={props.items[3].amount}
-				date={props.items[3].date}
-			/>
+			{props.items.map((expenseItem) => {
+				return (<ExpenseItem
+					title={expenseItem.title}
+					amount={expenseItem.amount}
+					date={expenseItem.date}
+				/>);
+			})}
 		</Card>
 	);
 };
