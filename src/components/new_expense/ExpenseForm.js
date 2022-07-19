e62df7e74c5a8ad849b384afbf79edddaac6f585
Manav Bhanot <manavb@amazon.com>
2022-07-18 18:49:13 -0700

    L54-L56: Working with multiple state updates and various ways to update the state

--- src/components/new_expense/ExpenseForm.js
+++ src/components/new_expense/ExpenseForm.js
@@ -1,20 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
+	// const [enteredTitle, setEnteredTitle] = useState("");
+	// const [enteredAmount, setEnteredAmount] = useState("");
+	// const [enteredDate, setEnteredDate] = useState("");
 
-  const titleChangeHandler = (event) => {
-    console.log(event);
-    console.log(event.target.value);
-  }
+	const [userInput, setUserInput] = useState({
+		enteredTitle: "",
+		enteredAmount: "",
+		enteredDate: "",
+	});
 
-  const amountChangeHandler = (event) => {
-  }
+	const titleChangeHandler = (event) => {
+		// setEnteredTitle(event.target.value);
 
-  const dateChangeHandler = (event) => {
+		// The below is not the best/good option to update the state. Because React schedules the state updates and there could be a chance
+		// that previous state values captured are obsolete.
+		// setUserInput({
+		//   ...userInput,
+		//   enteredTitle: event.target.value
+		// });
 
-  }
+		// This is the safer way to ensure that the state update takes the latest values from the previous state
+		setUserInput((prevState) => {
+			return { ...prevState, enteredTitle: event.target.value };
+		});
+	};
+
+	const amountChangeHandler = (event) => {
+		// setEnteredAmount(event.target.value);
+		// setUserInput({
+		// 	...userInput,
+		// 	enteredAmount: event.target.value,
+		// });
+
+		setUserInput((prevState) => {
+			return { ...prevState, enteredAmount: event.target.value };
+		});
+	};
+
+	const dateChangeHandler = (event) => {
+		// setEnteredDate(event.target.value);
+		// setUserInput({
+		// 	...userInput,
+		// 	enteredDate: event.target.value,
+		// });
+
+		setUserInput((prevState) => {
+			return { ...prevState, enteredDate: event.target.value };
+		});
+	};
 
 	return (
 		<form>
@@ -25,15 +62,25 @@ const ExpenseForm = () => {
 				</div>
 				<div className="new-expense__control">
 					<label>Amount</label>
-					<input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+					<input
+						type="number"
+						min="0.01"
+						step="0.01"
+						onChange={amountChangeHandler}
+					/>
 				</div>
 				<div className="new-expense__control">
 					<label>Date</label>
-					<input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+					<input
+						type="date"
+						min="2019-01-01"
+						max="2022-12-31"
+						onChange={dateChangeHandler}
+					/>
 				</div>
-        <div className="new-expense__actions">
-          <button type="Submit">Add Expense</button>
-        </div>
+				<div className="new-expense__actions">
+					<button type="Submit">Add Expense</button>
+				</div>
 			</div>
 		</form>
 	);
